7c0c59753d1a6379febf702a72829418ca5a76a1
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-11 19:06:25 +0700

    Test out the connection of store and saga

--- src/containers/MediaGalleryPage.js
+++ src/containers/MediaGalleryPage.js
@@ -1,18 +1,66 @@
 import React, { Component } from "react"
-import { flickrImages, shutterStockVideos } from "../api/index"
+// import PropTypes from "prop-types"
+
+import { connect } from "react-redux"
+import { searchMediaAction } from "../actions/mediaActions"
 
 // MediaGalleryPage Component
 class MediaGalleryPage extends Component {
-  // We want to get images and videos from the API right after our component renders.
+  // Dispatches *searchMediaAction* immediately after initial rendering.
+  // Note that we are using the dispatch method from the store to execute this task, courtesy of react-redux
   componentDidMount() {
-    flickrImages("rain").then((images) => console.log(images, "Images"))
-    shutterStockVideos("rain").then((videos) => console.log(videos, "Videos"))
+    this.props.dispatch(searchMediaAction("cloud"))
   }
 
   render() {
-    // TODO: Render videos and images here
-    return <div>MEDIAS</div>
+    console.log(this.props.images, "Images")
+    console.log(this.props.videos, "Videos")
+    console.log(this.props.selectedImage, "SelectedImage")
+    console.log(this.props.selectedVideo, "SelectedVideo")
+    return (
+      <div>
+        <div>
+          <h1>IMAGES</h1>
+          <ul>
+            {this.props.images &&
+              this.props.images.map((image) => {
+                return <li key={image.id}>{image.title}</li>
+              })}
+          </ul>
+        </div>
+        <div>
+          <h1>VIDEOS</h1>
+          <ul>
+            {this.props.videos &&
+              this.props.videos.map((video) => {
+                return <li key={video.id}>{video.description}</li>
+              })}
+          </ul>
+        </div>
+      </div>
+    )
   }
 }
 
-export default MediaGalleryPage
+// -----------------------------------------------------------------------------
+
+// Define PropTypes
+MediaGalleryPage.propTypes = {
+  // Define your PropTypes here
+}
+
+// -----------------------------------------------------------------------------
+
+// Subscribe component to redux store and merge the state into
+// component's props
+const fromStoreToProps = ({ images, videos }) => ({
+  images: images[0],
+  selectedImage: images.selectedImage,
+  videos: videos[0],
+  selectedVideo: videos.selectedVideo
+})
+
+// -----------------------------------------------------------------------------
+
+// connect method from react-redux connects the component with redux store
+export default connect(fromStoreToProps)(MediaGalleryPage)
